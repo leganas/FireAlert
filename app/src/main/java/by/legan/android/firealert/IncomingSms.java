ffaf86b0e87d1615304a16c3e400e3c0e5ff3870
AndreiLS <andrei@legan.by>
2021-02-24 16:37:55 +0300

    Android 10 , ёбаный гугл

--- app/src/main/java/by/legan/android/firealert/IncomingSms.java
+++ app/src/main/java/by/legan/android/firealert/IncomingSms.java
@@ -1,20 +1,40 @@
 package by.legan.android.firealert;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.andreyls.mytestprojectsolo.R;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 import static by.legan.android.firealert.BoilerControlAct.ID_BOILER;
 
 /**
@@ -23,6 +43,25 @@ import static by.legan.android.firealert.BoilerControlAct.ID_BOILER;
 
 public class IncomingSms extends BroadcastReceiver {
     public static final String SMS_MSG =  "by.legan.android.firealert.sms_msg";
+    private static String channel_name = "IncomingSmsReceiver";
+    private static String channel_description = "";
+
+    private void createNotificationChannel(Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = channel_name;
+            String description = channel_description;
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(SMS_MSG, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -46,25 +85,21 @@ public class IncomingSms extends BroadcastReceiver {
                     Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
 
 
+
                     // Show Alert
                     int duration = Toast.LENGTH_LONG;
                     Toast toast = Toast.makeText(context,
                             "senderNum: "+ senderNum + ", message: " + message, duration);
-                    toast.show();
+//                    toast.show();
 
                     BoilerLab boilerLab = BoilerLab.get(context);
                     ArrayList<Boiler> boilers;
                     boilers = (ArrayList<Boiler>) boilerLab.getBoilers();
 
-                    for (int b=0;i<boilers.size();b++) {
-                        Log.d("SmsReceiver", "Проверка номера : " + boilers.get(b).getAlert_number());
+                    for (int b=0; b < boilers.size();b++) {
+                        if (boilers.get(b).getAlert_number() != null) Log.d("SmsReceiver", "Проверка номера : " + boilers.get(b).getAlert_number());
                         if (boilers.get(b).getAlert_number() != null) {
                             if (boilers.get(b).getAlert_number().equals(phoneNumber)) {
-                                Intent in = new Intent(context, AlertActivity.class);
-                                in.putExtra(ID_BOILER, boilers.get(b).get_id());
-                                in.putExtra(SMS_MSG, message);
-                                in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
                                 // Дальше проверяем есть ли у нас обозначеные эвенты
                                 // если есть и нужно открывать активность и реагировать то делаем это
                                 // если нет то забиваем хуй
@@ -80,7 +115,43 @@ public class IncomingSms extends BroadcastReceiver {
                                             flag = true;
                                     }
                                 }
-                                if (flag) context.startActivity(in);
+                                if (flag){
+                                    Intent in = new Intent(context, AlertActivity.class);
+                                    in.putExtra(ID_BOILER, boilers.get(b).get_id());
+                                    in.putExtra(SMS_MSG, message);
+                                    in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    context.startActivity(in); // Для Android 10, требует выставления в настройках приложения Поверх всех окон
+
+
+
+/*                                    Log.d("SmsReceiver", "StartNotification");
+                                    createNotificationChannel(context);
+                                    Intent fullScreenIntent = new Intent(context, AlertActivity.class);
+                                    fullScreenIntent.setAction(Intent.ACTION_VIEW);
+                                    fullScreenIntent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+                                    fullScreenIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    fullScreenIntent.putExtra(ID_BOILER, boilers.get(b).get_id());
+                                    fullScreenIntent.putExtra(SMS_MSG, message);
+
+                                    PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(context, 0,
+                                            fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                                    NotificationCompat.Builder notificationBuilder =
+                                            new NotificationCompat.Builder(context, SMS_MSG)
+                                                    .setSmallIcon(R.drawable.alarme)
+                                                    .setContentTitle("Incoming call")
+                                                    .setContentText("(919) 555-1234")
+                                                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                                    .setCategory(NotificationCompat.CATEGORY_CALL)
+                                                    .setFullScreenIntent(fullScreenPendingIntent, true);
+
+                                    Notification incomingCallNotification = notificationBuilder.build();
+
+                                    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                                    // notificationId is a unique int for each notification that you must define
+                                    notificationManager.notify(1010, incomingCallNotification);*/
+
+                                }
                             }
                         }
 
@@ -93,6 +164,7 @@ public class IncomingSms extends BroadcastReceiver {
 
         }
     }
+
 
     @NonNull
     private Gson getGson() {
